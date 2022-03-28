083e60bc9cff13db7390cb88b3e2e513a3df961d
Nikolai Lisenko <kollysenko@gmail.com>
2022-03-28 23:04:56 +0500

    added coroutines

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -3,7 +3,9 @@ package com.example.shoppinglist.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
@@ -14,24 +16,28 @@ class ShopListRepositoryImpl(application: Application) : ShopListRepository {
     private val shopListDAO = AppDatabase.getInstance(application).shopListDao()
     private val mapper = ShopListMapper()
 
-    override fun addShopItem(shopItem: ShopItem) {
+    override suspend fun addShopItem(shopItem: ShopItem) {
        shopListDAO.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
-    override fun deleteShopItem(shopItem: ShopItem) {
+    override suspend  fun deleteShopItem(shopItem: ShopItem) {
         shopListDAO.deleteShopItem(shopItem.id)
     }
 
-    override fun editShopItem(shopItem: ShopItem) {
+    override suspend  fun editShopItem(shopItem: ShopItem) {
         shopListDAO.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
-    override fun getShopItem(shopItemId: Int): ShopItem {
+    override suspend  fun getShopItem(shopItemId: Int): ShopItem {
         val dbModel = shopListDAO.getShopItem(shopItemId)
         return mapper.mapDbModelToEntity(dbModel)
     }
 
-    //TODO() override fun getShopList(): LiveData<List<ShopItem>> = shopListDAO.getShopList()
+   override fun getShopList(): LiveData<List<ShopItem>> = Transformations.map(
+       shopListDAO.getShopList()
+   ){
+       mapper.mapListDbModelToListEntity(it)
+   }
 
 
 }
